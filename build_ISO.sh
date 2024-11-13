e09463539df849072d82a5ef1cde8f4d62ff1d19
Zoey Ahmed <103337562+ZoeTheTransHoe@users.noreply.github.com>
2024-11-13 22:25:26 +0000

    Add Zola, remove gnome tour /help, update build.sh

--- build_ISO.sh
+++ build_ISO.sh
@@ -1,3 +1,5 @@
 mkdir ./iso-output
+sudo -i 
+rm -r ./iso-output/*
 sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer ghcr.io/jasonn3/build-container-installer:latest IMAGE_REPO=ghcr.io/zoethetranshoe IMAGE_NAME=zoeys-first-image IMAGE_TAG=latest VARIANT=Silverblue VERSION=41
 sudo chown zoey ./iso-output/deploy.iso 
