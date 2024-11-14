2dbfeb7b5ccb56a0f37d343fc696d56bb44b9167
Zoey Ahmed <103337562+ZoeTheTransHoe@users.noreply.github.com>
2024-11-14 16:33:59 +0000

    skibidi

--- build_ISO.sh
+++ build_ISO.sh
@@ -1,5 +1,4 @@
 mkdir ./iso-output
-sudo -i 
-rm -r ./iso-output/*
+rm -rf ./iso-output/*
 sudo podman run --rm --privileged --volume ./iso-output:/build-container-installer/build --security-opt label=disable --pull=newer ghcr.io/jasonn3/build-container-installer:latest IMAGE_REPO=ghcr.io/zoethetranshoe IMAGE_NAME=zoeys-first-image IMAGE_TAG=latest VARIANT=Silverblue VERSION=41
 sudo chown zoey ./iso-output/deploy.iso 
